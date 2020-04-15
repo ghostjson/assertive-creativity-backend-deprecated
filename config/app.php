3c0ec225fb12d35ffacb10caf31976cfd0c56a65
Akhil Ravindran <imakhilravindran@gmail.com>
2020-04-15 17:50:53 +0530

    add site url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://assertivecreativity.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
