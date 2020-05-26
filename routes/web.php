4c2b0cc0cb843d870dbbe836e16078a3557cff1f
Akhil Ravindran <imakhilravindran@gmail.com>
2020-05-26 20:35:57 +0530

    add login page

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,11 @@ Route::get('/pages/about', function () {
 Route::get('/form/forms', 'FormController@formsView');
 
 
-Route::get('/form/form-creator', function(){
+Route::get('/form/form-creator', function () {
     return view('form.form-creator');
 });
 
-Route::get('/form/form-view', function(){
+Route::get('/form/form-view', function () {
     return view('form.form-view');
 });
 
@@ -37,9 +37,12 @@ Route::get('/form/remove/{id}', 'FormController@formRemove');
 Route::get('/form/{id}', 'FormController@getFormData');
 
 
+// login
+Route::get('/login', function () {
+    return view('login');
+});
+
+
 //api section NON PROPER WAY
 Route::post('/api/form/save', 'FormController@save');
 Route::post('/api/form/update', 'FormController@update');
-
-
-
