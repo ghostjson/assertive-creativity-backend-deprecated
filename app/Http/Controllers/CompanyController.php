c0e55d5272c67a4af454a24bfd25ba21835628b0
Akhil Ravindran <imakhilravindran@gmail.com>
2020-07-01 21:31:19 +0530

    company middleware fix

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -13,7 +13,7 @@ class CompanyController extends Controller
 {
     public function __construct()
     {
-        $this->middleware = ['auth.admin'];
+        $this->middleware('auth.admin');
     }
 
     function index()
