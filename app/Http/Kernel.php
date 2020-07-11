8928b7ea3bba80506abc533e00bbea2e98df7e13
Akhil Ravindran <imakhilravindran@gmail.com>
2020-07-11 22:34:33 +0530

    remove space in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,7 +32,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
@@ -64,7 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
         'auth.admin' => \App\Http\Middleware\IsAdminMiddleware::class,
         'auth.vendor' => \App\Http\Middleware\isVendorMiddleware::class
     ];
